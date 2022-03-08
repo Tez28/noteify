751ec59ef83b6a6c9b7939d8ad243a8b5e227c7a
Tez28 <courtezcannady@gmail.com>
2022-03-08 10:58:47 -0500

    add app.use

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const apiroutes = require('./routes/apiroutes')
+const htmlroutes = require('./routes/htmlroutes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// add routes
+app.use("/", htmlroutes);
+
+app.use("/api", apiroutes);
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
